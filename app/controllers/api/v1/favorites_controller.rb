0f0fbd35d526501951e04bd50fc9144b47544b4a
github username <github email address>
2019-06-19 11:12:30 -0400

    tweaking editing favorites

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -12,13 +12,22 @@ class Api::V1::FavoritesController < ApplicationController
   def update
     fav = Favorite.find(params[:id])
 
-
-    fav.tags ?
-      fav.tags += ', ' + params[:favorite][:tags]
-      :
+    fav.tags === "" ?
       fav.tags = params[:favorite][:tags]
+      :
+      fav.tags += ', ' + params[:favorite][:tags]
 
     fav.save
+
+    render json: fav
+  end
+
+  def removeTag
+    fav = Favorite.find(params[:id])
+
+    tags = fav.tags.downcase.split(", ") - [params[:remove].downcase]
+
+    fav.update(tags: tags.join(", "))
 
     render json: fav
   end
